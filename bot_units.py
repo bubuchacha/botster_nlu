1f49898f645a7fdcd57d9a2f3337edfbcecbe2b0
bubuchacha <patricryanlee@gmail.com>
2017-11-07 10:45:24 +0800

    added spell checker

--- bot_units.py
+++ bot_units.py
@@ -3,6 +3,24 @@ from rasa_nlu.config import RasaNLUConfig
 from rasa_nlu.model import Metadata, Interpreter
 from klein import Klein
 import json
+from enchant.checker import SpellChecker
+from enchant import DictWithPWL
+import difflib
+
+
+def get_best_word(chkr, word):
+  best_words = []
+  best_ratio = 0
+  a = set(chkr.suggest(word)[0:8])
+  for b in a:
+    tmp = difflib.SequenceMatcher(None, word, b).ratio()
+    if tmp > best_ratio:
+      best_words = [b]
+      best_ratio = tmp
+    elif tmp == best_ratio:
+      best_words.append(b)
+  print best_words
+  return best_words[0]
 
 interpreter = Interpreter.load('models/units/current',RasaNLUConfig('config_units.json'))
     
@@ -10,6 +28,9 @@ interpreter = Interpreter.load('models/units/current',RasaNLUConfig('config_unit
 class Bot:
     '''bot'''
     app = Klein()
+    my_dict = DictWithPWL("en_US", "words.txt")
+    chkr = SpellChecker(my_dict)
+
 
     def __init__(self):
         self.test = 'hi'
@@ -21,7 +42,13 @@ class Bot:
         request_params = {key.decode('utf-8', 'strict'): value[0].decode('utf-8', 'strict')
                               for key, value in request.args.items()}
         text = str(request_params['q']).strip()
-        resp = interpreter.parse(unicode(text, encoding="utf-8"))
+        self.chkr.set_text(text)
+        
+        for err in self.chkr:
+          err.replace(get_best_word(self.chkr, err.word))
+        
+        spell_checked = self.chkr.get_text()
+        resp = interpreter.parse(unicode(spell_checked, encoding="utf-8"))
         
         print(resp)
         
@@ -29,6 +56,7 @@ class Bot:
             reply = {"intent": resp['intent'], "entities": resp['entities']}
         else:
             reply = {"intent": { "name": "None" }, "entities": ""}
+
 
         return json.dumps(dict(reply), indent=4)
 
